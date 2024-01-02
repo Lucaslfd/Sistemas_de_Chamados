352352a432b9293b178e40f6b344f7896b5d01fb
Lucaslfd <empresa.lfd@gmail.com>
2024-01-02 19:29:30 -0300

    Criando componente title

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import SingIn from '../pages/SingIn';
 import SingUp from '../pages/SingUp';
 import Dashboard from '../pages/Dashboard';
-import Private, {} from './Private';
+import Profile from '../pages/Profile';
 
+import Private from './Private';
 function RoutesApp() {
     return(
         <Routes>
             <Route path='/' element={ <SingIn/> }/>
             <Route path='/register' element={ <SingUp/> }/>
             <Route path='/dashboard' element={<Private><Dashboard/></Private> }/>
+            <Route path='/profile' element={ <Private> <Profile/> </Private> }/>
         </Routes>
     )
 }
